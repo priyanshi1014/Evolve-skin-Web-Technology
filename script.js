5d09a2a45743bc7b0a898dcfc38b5c8af0ee251b
priyanshi1014 <priyanshi.panchal10@gmail.com>
2025-07-26 13:21:17 +0530

    Initial commit - Web Technology Project

---
+++ script.js
@@ -0,0 +1,5 @@
+        function addToCart(productName) {
+            alert(productName + " has been added to your cart!");
+            // Here, you can add code to actually handle the cart logic, such as updating a cart counter or storing items in localStorage.
+        }
+        
